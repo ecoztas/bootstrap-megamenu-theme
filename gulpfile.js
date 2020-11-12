cd5d05c4c16cdaac6cc0d3f06fed5c4229293410
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2020-11-12 14:41:22 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,57 @@
 /**
- * SASS-KIT for SASS development.
- * 
- * 
- * 
- * @package     sass-kit
+ * Bootstrap Mega Menu - Theme
+ *
+ *
+ *
+ * @package     bootstrap-megamenu-theme
  * @author      ÖZTAŞ, Emre Can <me@emrecanoztas.com>
  * @copyright   2020
  * @license     MIT
- * @link        https://github.com/ecoztas/sass-kit
+ * @link        https://github.com/ecoztas/bootstrap-megamenu-theme
  * @since       v1.0
  */
 
 // DEFINE: Package(s).
 // =============================================================================
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin');
-var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var jade = require("gulp-jade");
+var cssmin = require("gulp-cssmin");
+var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
 
 // TASK: Default
 // =============================================================================
-gulp.task('default', function () {
-    console.log('Works fine!');
+gulp.task("default", function () {
+    console.log("Works fine!");
 });
 
 // TASK: SASS
 // =============================================================================
-gulp.task('sass', function () {
-    return (
-        gulp.src('public/template/sass-kit/theme.scss')
-            .pipe(plumber())
-            .pipe(sass())
-            .pipe(gulp.dest('public/styles'))
-            .pipe(cssmin())
-            .pipe(rename({
-                suffix: '.min'
-            }))
-            .pipe(gulp.dest('public/styles'))
-    );
+gulp.task("sass", function () {
+    return gulp
+        .src("public/template/sass-kit/theme.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest("public/styles"))
+        .pipe(cssmin())
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(gulp.dest("public/styles"));
+});
+
+// TASK: JADE
+// =============================================================================
+gulp.task("jade", function () {
+    return gulp
+        .src("public/template/jade-kit/*.jade")
+        .pipe(
+            jade({
+                pretty: true,
+            })
+        )
+        .pipe(gulp.dest("./"));
 });
